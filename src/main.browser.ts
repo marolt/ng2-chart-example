485b67c8e26ea4d46d0b1d4710472e3c3f7a778e
Žiga Marolt <ziga@merchbee.com>
2017-02-10 15:10:18 +0100

    Dependency updates

--- src/main.browser.ts
+++ src/main.browser.ts
@@ -1,32 +1,3 @@
-// import './polyfills.browser';
-// import './rxjs.imports';
-//
-// declare var ENV: string;
-//
-// import { enableProdMode } from '@angular/core';
-// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-// import { bootloader } from '@angularclass/hmr';
-// import { AppModule } from './app/app.module';
-//
-// if ('production' === ENV) {
-//   enableProdMode();
-// }
-//
-// export function main() {
-//   return platformBrowserDynamic().bootstrapModule(AppModule)
-//     .catch(err => console.log(err));
-// }
-//
-// export function bootstrapDomReady() {
-//   document.addEventListener('DOMContentLoaded', main);
-// }
-//
-// // bootstrapDomReady();
-//
-// bootloader(main);
-
-
-
 import './polyfills.browser';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
